d51af66b4871d244abb43925e1725fddb2cd3962
Wagner <wagnerdossantos2311@gmail.com>
2026-02-12 13:42:10 -0300

    implementei novas formulas matematicas e refatorei o codigo

--- src/main/java/com/wagner/rest_with_spring_boot_and_java_wagner/controller/MathController.java
+++ src/main/java/com/wagner/rest_with_spring_boot_and_java_wagner/controller/MathController.java
@@ -5,40 +5,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wagner.rest_with_spring_boot_and_java_wagner.exception.UnsupportedMathOperetionException;
+import com.wagner.rest_with_spring_boot_and_java_wagner.util.SimpleMath;
+import com.wagner.rest_with_spring_boot_and_java_wagner.util.VerificarNumber;
 
 @RestController
 @RequestMapping("/Math")
 public class MathController {
 
-    @RequestMapping("/sum/{numberOne}/{numberTwo}")
-    public Double sum(
-            @PathVariable("numberOne") String numberOne,
-            @PathVariable("numberTwo") String numberTwo
+    SimpleMath sm = new SimpleMath();
 
-    ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
-            throw new UnsupportedMathOperetionException("Coloque um valor numerico");
-        return Double.parseDouble(numberOne.replace(",", ".")) + Double.parseDouble(numberTwo.replace(",", "."));
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        verificar(numberTwo, numberOne, numberTwo);
+        return sm.sum(numberOne, numberTwo);
     }
 
     @RequestMapping("/sub/{numberOne}/{numberTwo}")
-    public Double sub(
-            @PathVariable("numberOne") String numberOne,
-            @PathVariable("numberTwo") String numberTwo
-
-    ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
-            throw new UnsupportedMathOperetionException("Coloque um valor numerico");
-        return Double.parseDouble(numberOne.replace(",", ".")) - Double.parseDouble(numberTwo.replace(",", "."));
+    public Double sub(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        verificar(numberTwo, numberOne, numberTwo);
+        return sm.sub(numberOne, numberTwo);
     }
 
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null || strNumber.isEmpty())
-            return false;
-        String number = strNumber.replace(",", ".");
-        if (number.matches("[-+]?[0-9]*\\.?[0-9]+"))
-            return true;
-        return false;
+    @RequestMapping("mult/{numberOne}/{numberTwo}")
+    public Double mult(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        verificar(numberTwo, numberOne, numberTwo);
+        return sm.mult(numberOne, numberTwo);
+    }
+
+    @RequestMapping("div/{numberOne}/{numberTwo}")
+    public Double div(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        verificar(numberTwo, numberOne, numberTwo);
+        return sm.div(numberOne, numberTwo);
+    }
+
+    @RequestMapping("raiz/{number}")
+    public Double raiz(@PathVariable("number") String number) {
+        if (!VerificarNumber.isNumeric(number))
+            throw new UnsupportedMathOperetionException("Coloque um numero adequado");
+        return sm.raiz(number);
+    }
+
+    private void verificar(String menssage, String numberOne, String numberTwo) {
+        if (!VerificarNumber.isNumeric(numberOne) || !VerificarNumber.isNumeric(numberTwo))
+            throw new UnsupportedMathOperetionException(menssage);
+
     }
 
 }
